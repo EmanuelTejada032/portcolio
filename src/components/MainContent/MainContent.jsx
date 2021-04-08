960378b23e4f1255b390035922cebec6ed615c92
Emanuel <emanueltejada31295@gmail.com>
2021-04-07 22:33:40 -0400

    structure and add all data

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -1,20 +1,49 @@
 import React from 'react'
-import background from '../../img/herobgtest.jpg'
+// import background from '../../img/herobgtest.jpg'
+// import bootstrapLogo from '../../img/bootstrap-4.svg'
+
+import { SvgLogos, Projects } from '../../data'
+
 const MainContent = () => {
+
     return (
       <>
-        <div className="hero-section" style={{backgroundImage: `url(${background})`, backgroundPosition: 'center'}} >
-          <div className="hero-heading">
+        <header>
             <h2>I'm Emanuel Tejada</h2>
-            <h4>I'm a web developer</h4>
-          </div>
-        </div>
-        <div className="about-section">
-          <div className="about-content">
-            <h2>About Me</h2>
-            <p>Hi! My name is Emanuel and I'm currently a software developer graduated from ITSC, I'm also a selft taugth web developer. Learning to code has been dificult journey, however I keep putting effort and dedicating time every day  learning to code and become a better web developer and improving my problem solving skills to be able to understand and work with backend technolgies in the web development industry and taking my web design knowledge next level to bring value to any future employer or customer that I could have. I like to work in both front-end and backend development process for websites applications. Even if it's difficult learning to code, I belive in my habilities to learn and keep going this road much further</p>
-          </div>
-        </div>
+            <h4>Web developer</h4>
+        </header>
+        <section>
+          <section className='about'>
+          <h2>About Me</h2>
+          <p>Hi! My name is Emanuel and I'm currently a software developer graduated from the ITSC, I'm also a self taugth web developer.</p>
+          <p>Learning to code has been dificult a journey, however I keep putting effort and dedicating time every day learning new skills  and improving what I already kno to bring value to any future employer or customer that I could have. Even if it's difficult learning to code, I belive in my habilities to learn and keep going this road much further</p>
+          </section>
+          <section className='skills'>
+                   {SvgLogos.map( (logo, i) => (
+                     <div key={i} className='skill'>
+                       <img  src={logo.url} alt={logo.name} />
+                     </div>  
+                   ))}
+          </section>
+          <section className='projects'>   
+              {Projects.map( (project, i) => (
+                <div key={i} className='project'>
+                  <div className='project-info'>
+                    <h4>{project.title}</h4>
+                    <p>{project.description}</p>
+                    <div className='tech-icons'>
+                      {project.usedTech.map((icon, i) =>(
+                        <p className='tech-icon'>this is a tech icon</p>
+                      ))}
+                    </div>
+                    <a href={project.github}>githubButton</a>
+                    <a href={project.deployed}>deployedVersionButton</a>
+                  </div>
+                  <p>image</p>
+                </div>
+              ))}
+          </section>
+        </section>
       </>
     )
 }
---
+++ src/data.js
@@ -0,0 +1,98 @@
+const html5 = 'https://res.cloudinary.com/emanueltejada/image/upload/v1617844514/Test/html5_kt9kol.svg'
+const css3 = 'https://res.cloudinary.com/emanueltejada/image/upload/v1617844514/Test/css3_degk7j.svg'
+const javascript = 'https://res.cloudinary.com/emanueltejada/image/upload/v1617839889/Test/logo-javascript_ipm3za.svg'
+const bootstrap = 'https://res.cloudinary.com/emanueltejada/image/upload/v1617833898/Test/bootstrap-4_lgpmht.svg'
+const react = 'https://res.cloudinary.com/emanueltejada/image/upload/v1617833898/Test/react-2_z8tmub.svg'
+const figma = 'https://res.cloudinary.com/emanueltejada/image/upload/v1617839889/Test/figma-1_ael2as.svg'
+const materialUI = 'https://res.cloudinary.com/emanueltejada/image/upload/v1617839889/Test/material-ui-1_khpiqs.svg'
+const nodeJs = 'https://res.cloudinary.com/emanueltejada/image/upload/v1617844514/Test/nodejs-icon_vpzqco.svg'
+const mongoDB = 'https://res.cloudinary.com/emanueltejada/image/upload/v1617844514/Test/mongodb-icon-1_yd86rk.svg'
+
+
+export const SvgLogos = [
+    {   name: 'Bootstrap logo',
+        url: bootstrap,
+    },
+    {
+        name: 'Html5 logo',
+        url: html5
+    },
+    {
+        name: 'React logo',
+        url: react
+    },
+    {   name: 'Javascript logo',
+        url: javascript
+    },
+    {
+        name: 'Figma logo',
+        url: figma
+    },
+    {
+        name: 'Mongo DB logo',
+        url: mongoDB
+    },
+    {
+        name: 'Node Js logo',
+        url: nodeJs
+    },
+    {
+        name: 'Material UI logo',
+        url: materialUI
+    },
+    {
+        name: 'CSS3 UI logo',
+        url: css3
+    },   
+]
+
+export const Projects = [
+    {
+        title: 'Yelpcamp',
+        description: 'Some description not specified yet',
+        usedTech: [
+            html5,
+            css3,
+            javascript
+        ],
+        image: 'imageurl',
+        github: 'https://github.com/EmanuelTejada032',
+        deployed: 'https://github.com/EmanuelTejada032'
+    },
+    {
+        title: 'Shopping Cart',
+        description: 'Some description not specified yet',
+        usedTech: [
+            html5,
+            css3,
+            javascript
+        ],
+        image: 'imageurl',
+        github: 'https://github.com/EmanuelTejada032',
+        deployed: 'https://github.com/EmanuelTejada032'
+    },
+    {
+        title: 'FakeFlix',
+        description: 'Some description not specified yet',
+        usedTech: [
+            html5,
+            css3,
+            javascript
+        ],
+        image: 'imageurl',
+        github: 'https://github.com/EmanuelTejada032',
+        deployed: 'https://github.com/EmanuelTejada032'
+    }
+]
+
+export const  socialMedias = [
+    {
+        url: 'Linkedink'
+    },
+    {
+        url: 'Twitter'
+    },
+    {
+        url: 'Github'
+    },
+]
