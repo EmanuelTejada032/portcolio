fdc95ba31468d45b72543d810fdcf39200fffc39
Emanuel <emanueltejada31295@gmail.com>
2021-04-15 22:35:46 -0400

    Test  Netlify forms

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
 import MainContent from './components/MainContent/MainContent'
+import Form from './components/Form/Form'
 
 function App() {
   return (
     <>
         <Navbar />
         <MainContent />
+        <Form />
         <Footer />
     </>
   );
