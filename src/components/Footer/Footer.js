dc78c97639c94c0ff4be09ba0239a115fee3450c
Emanuel <emanueltejada31295@gmail.com>
2021-04-17 15:06:17 -0400

    fix bugs

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import { MenuItems } from '../Navbar/MenuItems'
+import { Link} from "react-scroll";
 import './styles.css'
 
 const Footer = () => {
+    
     return (
         <div className='footer'>
                 <ul>
                    {MenuItems.map( (item, id) => (
-                       <li key={id}><a href={item.url}>{item.title}</a></li>
+                       <li key={id} ><Link  spy={true}
+                       smooth={true}
+                       offset={item.url === 'about'? 1 : -120}
+                       duration={800} 
+                       activeClass='active' 
+                       to={item.url}>{item.title}</Link></li>
                    ))}
                 </ul>
         </div>
